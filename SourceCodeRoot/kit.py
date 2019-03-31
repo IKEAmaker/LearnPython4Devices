d7d7cef00ae99f50d3645c527357dfa637cc07dd
marcushj <marcus@samora.se>
2019-03-31 15:02:27 +0200

    add suport for max7219

--- SourceCodeRoot/kit.py
+++ SourceCodeRoot/kit.py
@@ -1,17 +1,16 @@
-import busio
-from adafruit_ht16k33 import matrix
-from analogio import AnalogIn
-from digitalio import DigitalInOut, Direction, Pull
-import bitmapfont
 import board
+import busio
+import digitalio
+from analogio import AnalogIn
+
 
 class Joystick:
     def __init__(self, board):
         self.upDown = AnalogIn(board.A3)
         self.rightLeft = AnalogIn(board.A4)
-        self.button = DigitalInOut(board.A0)
-        self.button.direction = Direction.INPUT
-        self.button.pull = Pull.UP
+        self.button = digitalio.DigitalInOut(board.A0)
+        self.button.direction = digitalio.Direction.INPUT
+        self.button.pull = digitalio.Pull.UP
         
         up_sum = 0.0
         right_sum = 0.0
@@ -23,8 +22,8 @@ class Joystick:
         self.up_cal = up_sum/1000
         self.right_cal = right_sum/1000
 
-        self.up_dir = 1
-        self.right_dir = 1
+        self.up_dir = -1
+        self.right_dir = -1
         
     
     def read_Up(self):
@@ -42,13 +41,45 @@ class Joystick:
     
     push = property(read_button)
 
+class Matrix:
+    def __init__(self, type):
+        self.type = type
+        _matrix.fill(0)
+        _matrix.show()
 
-i2c = busio.I2C(board.SCL, board.SDA)
-matrix = matrix.Matrix8x8(i2c, auto_write=False)
-matrix.fill(0)
-matrix.show()
+    def fill(self, color):
+        _matrix.fill(color)
         
-font = bitmapfont.BitmapFont(8, 8, matrix.pixel)
-font.init()
+    def show(self):
+        _matrix.show()
 
+    def pixel(self, x, y, color):
+        _matrix.pixel(x, y, color)
+        
+    def __setitem__ (self, coords, color):
+        self.pixel(coords[0], coords[1], color)
+        
+    def text(self, str, x, y, color):
+        if self.type == 'i2c':
+            font.text(str, x, y, color)
+        elif self.type =='spi':
+            _matrix.text(str, x, y, color)
+
+try:
+    i2c = busio.I2C(board.SCL, board.SDA)
+    from adafruit_ht16k33.matrix import Matrix8x8
+    import bitmapfont
+    _matrix = Matrix8x8(i2c, auto_write=False)
+    font = bitmapfont.BitmapFont(8, 8, _matrix.pixel)
+    font.init()
+    type = 'i2c'
+except:
+    from adafruit_max7219.matrices import Matrix8x8
+    cs = digitalio.DigitalInOut(board.D2)
+    spi = busio.SPI(board.SCK, MOSI=board.MOSI, )
+    _matrix = Matrix8x8(spi, cs)
+    _matrix.brightness(9)
+    type = 'spi'
+
+matrix = Matrix(type)
 joystick = Joystick(board)
